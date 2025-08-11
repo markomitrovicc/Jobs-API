4092894a72d58b6b195bab627a00a7a67abb024c
markomitrovicc <mitrovicmarkoit@gmail.com>
2025-08-11 02:18:14 +0200

    Added dummy route due to deployment issues

--- app.js
+++ app.js
@@ -33,6 +33,10 @@ app.use(cors());
 app.use(xss());
 
 // routes
+app.use('/', (req,res) => {
+  res.send('Jobs API'); // Dummy route to test if deployment works
+})
+
 app.use('/api/v1/auth', authRouter);
 //app.use('/api/v1/jobs', authMiddleware, jobsRouter);
 app.use('/api/v1/jobs', jobsRouter); //Temporary change, to test if it works when deployed
